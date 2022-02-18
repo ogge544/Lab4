df8d2374700f3b1afe9e2868a0b353c5b59d1624
Användaren <Anvndaren@DESKTOP-DQIG29E.lan>
2022-02-18 12:47:28 +0100

    First Commit!

---
+++ Lab4/src/lab4/GomokuMain.java
@@ -0,0 +1,25 @@
+package lab4;
+
+import lab4.client.GomokuClient;
+import lab4.gui.GomokuGUI;
+import lab4.data.GomokuGameState;
+
+public class GomokuMain {
+
+	public static void main(String[] argv) throws ArrayIndexOutOfBoundsException {
+		if(argv.length == 0) {			
+			argv[0] = "4000";
+		}else if(argv.length > 1) {
+			throw new ArrayIndexOutOfBoundsException("To many portnumbers!");
+		}
+		
+		
+		
+		int portnr = Integer.parseInt(argv[0]);
+		GomokuClient client = new GomokuClient(portnr);
+		GomokuGameState gamestate = new GomokuGameState(client);
+		GomokuGUI gui = new GomokuGUI(gamestate, client);
+
+	}
+
+}
